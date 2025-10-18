fb754f5f3dcb4eb0befce35070e88d7c1b6a9f11
Haouo <q18131500@gs.ncku.edu.tw>
2025-10-18 15:26:34 +0800

    WIP

--- cmake/riscv.cmake
+++
@@ -1,8 +0,0 @@
-# set compiler by  riscv-gnu-toolchain
-set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
-set(CMAKE_ASM_COMPILER riscv32-unknown-elf-gcc)
-set(CMAKE_OBJDUMP riscv32-unknown-elf-objdump)
-set(CMAKE_OBJCOPY riscv32-unknown-elf-objcopy)
-
-# set common compile flags
-set(COMMON_COMPILE_FLAG -march=rv32i -mabi=ilp32 -ffreestanding -fno-builtin -Wall -Werror)
